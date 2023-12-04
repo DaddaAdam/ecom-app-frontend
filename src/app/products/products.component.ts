12f218d1a5f3a2537ba43fa21a077d281e1a11de
DaddaAdam <daddaadam@gmail.com>
2023-12-04 18:12:09 +0100

    added products table

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    FormsModule
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  products: Array<any> = [
+    {id: 1, name: 'Computer', price: 9000, checked: false},
+    {id: 2, name: 'Printer', price: 6000, checked: true},
+    {id: 3, name: 'Smartphone', price: 12000, checked: false},
 
+  ]
+
+  handleCheckProduct(product: any) {
+    product.checked = !product.checked;
+  }
 }
