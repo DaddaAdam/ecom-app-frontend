ed838d00c50e74cb79f51d7898920188ab43f42f
DaddaAdam <daddaadam@gmail.com>
2023-12-05 16:56:02 +0100

    added app-navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  actions: Array<any> = [
-    {title: 'Home', route: '/home', icon: 'house'},
-    {title: 'Products', route: '/products', icon: 'search'},
-    {title: 'Add Product', route: '/newProduct', icon: 'plus-circle'},
-  ]
 
-  currentAction: any;
-
-  setCurrentAction(action: any) {
-    this.currentAction = action;
-  }
 }
