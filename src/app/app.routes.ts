577999c46fee817e16db6fb6bc9f37c3ea5e6f30
DaddaAdam <daddaadam@gmail.com>
2023-12-05 20:59:23 +0100

    added autorization guard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,17 @@ import {EditProductComponent} from "./edit-product/edit-product.component";
 import {LoginComponent} from "./login/login.component";
 import {AdminTemplateComponent} from "./admin-template/admin-template.component";
 import {AuthenticationGuard} from "./guards/authentication.guard";
+import {AuthorizationGuard} from "./guards/authorization.guard";
+import {NotAuthorizedComponent} from "./not-authorized/not-authorized.component";
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {path: '', redirectTo: 'login', pathMatch: 'full' },
   {path: 'home', component: HomeComponent},
   {path: 'admin', component: AdminTemplateComponent, canActivate:[AuthenticationGuard], children: [
       {path: 'products', component: ProductsComponent},
-      {path: 'newProduct', component: NewProductComponent},
-      {path: 'editProduct/:id', component: EditProductComponent},
+      {path: 'newProduct', component: NewProductComponent, canActivate:[AuthorizationGuard]},
+      {path: 'editProduct/:id', component: EditProductComponent, canActivate:[AuthorizationGuard]},
+      {path: 'notAuthorized', component: NotAuthorizedComponent}
     ]},
   {path: 'login', component: LoginComponent},
 ];
--- src/app/guards/authorization.guard.ts
+++ src/app/guards/authorization.guard.ts
@@ -1,5 +1,23 @@
-import { CanActivateFn } from '@angular/router';
+import {Injectable} from "@angular/core";
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {Observable} from "rxjs";
+import {AppStateService} from "../app-state.service";
 
-export const authorizationGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorizationGuard{
+  constructor(private appState: AppStateService,
+              private router: Router) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if(this.appState.authState.roles.includes('ADMIN'))
+      return true;
+    else
+      this.router.navigate(['/admin/notAuthorized'])
+    return false;
+  }
+}
