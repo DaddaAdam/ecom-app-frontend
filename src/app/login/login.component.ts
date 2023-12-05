69785e0caabb0bba0928cb1d880ae65581eb1f12
DaddaAdam <daddaadam@gmail.com>
2023-12-05 20:34:02 +0100

    added authentication in auth service and in login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,22 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {Router} from "@angular/router";
+import {AuthService} from "../services/auth.service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgIf
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit{
   formLogin!: FormGroup;
+  errorMessage = undefined;
 
   ngOnInit(): void {
     this.formLogin = this.formBuilder.group({
@@ -22,12 +26,20 @@ export class LoginComponent implements OnInit{
   }
 
   constructor(private formBuilder: FormBuilder,
-              private router: Router) {
+              private router: Router,
+              private authService: AuthService) {
   }
 
   handleSubmit() {
-    if(this.formLogin.value.username === 'admin' && this.formLogin.value.password === 'admin') {
-      this.router.navigate(['/admin/products'])
+    let username:string = this.formLogin.value.username;
+    let password:string = this.formLogin.value.password;
+    this.authService.login(
+      username, password
+    ).then((data: any) => {
+      this.router.navigate(['/admin/products']);
     }
+    ).catch((error: any) => {
+      this.errorMessage = error;
+    });
   }
 }
