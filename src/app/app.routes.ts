2b7df977538c6c071f989cf6a36612248aef5b6f
DaddaAdam <daddaadam@gmail.com>
2023-12-05 20:48:36 +0100

    added authentication guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,12 @@ import {NewProductComponent} from "./new-product/new-product.component";
 import {EditProductComponent} from "./edit-product/edit-product.component";
 import {LoginComponent} from "./login/login.component";
 import {AdminTemplateComponent} from "./admin-template/admin-template.component";
+import {AuthenticationGuard} from "./guards/authentication.guard";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {path: 'home', component: HomeComponent},
-  {path: 'admin', component: AdminTemplateComponent, children: [
+  {path: 'admin', component: AdminTemplateComponent, canActivate:[AuthenticationGuard], children: [
       {path: 'products', component: ProductsComponent},
       {path: 'newProduct', component: NewProductComponent},
       {path: 'editProduct/:id', component: EditProductComponent},
